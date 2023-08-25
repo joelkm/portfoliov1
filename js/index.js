5066e283026a1b05720e1aee491c1712bcd753ce
JoelKM <joelkasmor16@gmail.com>
2023-08-25 18:22:16 +0200

    Finished

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const logo = document.getElementById('logo');
+const loading = document.getElementById('loading');
+
+window.addEventListener('load', () => {
+    loading.style.display = 'none';
+})
+
+logo.addEventListener('click', () => {
+    location.reload();
+})
+
+document.querySelectorAll('.nav-link').forEach((listitem) => {
+    listitem.addEventListener('click', () => {
+        console.log("click");
+        let topPosition = document.getElementById(listitem.dataset.page).offsetTop - 200;
+        window.scrollTo({
+            top: topPosition,
+            left: 0,
+            behavior: 'smooth'
+        })
+    });
+})
+
+function displayMobileNav(e) {
+    let x = document.getElementsByClassName("list");
+    if (x[0].style.display === "flex") {
+        x[0].style.display = "none";
+        hamburguer.classList.remove('ham-active');
+    } else {
+        x[0].style.display = "flex";
+        hamburguer.classList.add('ham-active');
+    }
+}
+
+const hamburguer = document.getElementsByClassName('hamburguer')[0];
+
+hamburguer.addEventListener('click', (e) => {
+    displayMobileNav();
+})
